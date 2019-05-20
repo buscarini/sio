4934e5bd23e549a68fdc72fa8a5f5fcf10866eb3
José Manuel Sánchez <buscarini@gmail.com>
2019-05-20 17:15:12 +0200

    Added profunctor

---
+++ Sources/SIO+Contravariant.swift
@@ -0,0 +1,16 @@
+//
+//  SIO+Contravariant.swift
+//  sio-iOS
+//
+//  Created by José Manuel Sánchez Peñarroja on 20/05/2019.
+//  Copyright © 2019 sio. All rights reserved.
+//
+
+import Foundation
+
+extension SIO {
+	/// pullback or contramap for Contravariant functors
+	public func pullback<S>(_ f: @escaping (S) -> (R)) -> SIO<S, E, A> {
+		return self.dimap(f, id)
+	}
+}
---
+++ Sources/SIO+Profunctor.swift
@@ -0,0 +1,19 @@
+//
+//  SIO+Profunctor.swift
+//  sio-iOS
+//
+//  Created by José Manuel Sánchez Peñarroja on 20/05/2019.
+//  Copyright © 2019 sio. All rights reserved.
+//
+
+import Foundation
+
+public extension SIO {
+	func dimap<S, B>(_ pre: @escaping (S) -> R, _ post: @escaping (A) -> B) -> SIO<S, E, B> {
+		return SIO<S, E, B>({ s, reject, resolve in
+			self.fork(pre(s), reject, { a in
+				resolve(post(a))
+			})
+		})
+	}
+}
