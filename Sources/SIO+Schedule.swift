eca1db276bc8d1fd3b9868dd2000adb6074a1020
José Manuel Sánchez <buscarini@gmail.com>
2019-05-20 17:00:23 +0200

    Added dispatch queue utils

---
+++ Sources/SIO+Schedule.swift
@@ -0,0 +1,37 @@
+//
+//  SIO+Schedule.swift
+//  sio-iOS
+//
+//  Created by José Manuel Sánchez Peñarroja on 20/05/2019.
+//  Copyright © 2019 sio. All rights reserved.
+//
+
+import Foundation
+
+public extension SIO {
+	func scheduleOn(_ queue: DispatchQueue) -> SIO<R, E, A> {
+		return SIO({ (env, reject, resolve) in
+			queue.async {
+				self.fork(env, reject, resolve)
+			}
+		})
+	}
+	
+	func forkOn(_ queue: DispatchQueue) -> SIO<R, E, A> {
+		return SIO({ (env, reject, resolve) in
+			self.fork(
+				env,
+				{ error in
+					queue.async {
+						reject(error)
+					}
+				},
+				{ result in
+					queue.async {
+						resolve(result)
+					}
+				}
+			)
+		})
+	}
+}
