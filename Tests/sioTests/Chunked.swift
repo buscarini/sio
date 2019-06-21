4a607fb61f931cf04abbe25f368111febe353cf5
José Manuel Sánchez <buscarini@gmail.com>
2019-06-21 12:50:36 +0200

    More tests

---
+++ Tests/sioTests/Chunked.swift
@@ -0,0 +1,48 @@
+//
+//  Chunked.swift
+//  Task
+//
+//  Created by José Manuel Sánchez Peñarroja on 13/3/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import sio
+
+class Chunked: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testChunks() {
+		let values = Array(1...10)
+		let tasks = values
+				.map { IO<Error, Int>.of($0) }
+				.map(delayed(1))
+		
+		let taskChunks = tasks.chunked(by: 5) // [[task1, …, task5],[task6, …, task10]]
+			.map { parallel($0) } // [ task[], task[] ]
+		
+		let expectation = self.expectation(description: "tasks completed")
+		
+		let now = Date()
+
+		sequence(taskChunks) // task[]
+			.fork((), { _ in
+				XCTFail()
+			},
+			{ results in
+				XCTAssert(results == values)
+				XCTAssert(-now.timeIntervalSinceNow > 2.0)
+				
+				expectation.fulfill()
+			})
+		
+		self.waitForExpectations(timeout: 2.5, handler: nil)
+    }
+}
---
+++ Tests/sioTests/Delay.swift
@@ -0,0 +1,55 @@
+//
+//  Delay.swift
+//  Task
+//
+//  Created by José Manuel Sánchez Peñarroja on 10/3/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import sio
+
+class Delay: XCTestCase {
+    func exampleError() -> Error {
+		return NSError(domain: "tests", code: 1, userInfo: nil)
+	}
+
+	func testDelay() {
+		let expectation = self.expectation(description: "task is delayed")
+		
+		let task = IO<Error, Int>.of(1)
+	
+		let now = Date()
+	
+		task.delay(1)
+			.fork((), { error in
+				XCTFail()
+			},
+			{ value in
+				XCTAssert(value == 1)
+				XCTAssert(-now.timeIntervalSinceNow > 0.9)
+				
+				expectation.fulfill()
+			})
+		
+		self.waitForExpectations(timeout: 2.0, handler: nil)
+	}
+	
+    func testDelayFail() {
+		let expectation = self.expectation(description: "task is delayed with failure")
+		
+		let task = IO<Error, Int>.rejected(self.exampleError())
+		let now = Date()
+
+		task.delay(1)
+			.fork((), { error in
+				XCTAssert(-now.timeIntervalSinceNow > 0.9)
+				expectation.fulfill()
+			},
+			{ value in
+				XCTFail()
+			})
+		
+		self.waitForExpectations(timeout: 2.0, handler: nil)
+	}
+}
