9858630bd2d9f26c132fc5e397444e8dc3038d70
José Manuel Sánchez <buscarini@gmail.com>
2019-08-14 16:57:20 +0200

    Added more random tests

--- Tests/SioEffectsTests/SioRandomTests.swift
+++ Tests/SioEffectsTests/SioRandomTests.swift
@@ -21,15 +21,44 @@ class SIORandomTests: XCTestCase {
 		XCTAssert(gen.next() == 694254729704690381)
 	}
 	
-	func testRandom() {
+	func testRandomInt() {
 		let finish = expectation(description: "finished")
 		
 		let gen = LCRNG(seed: 1)
 
-		Random.Default.randomInt(0...9).fork(.init(.init(gen)), absurd, { num in
-			XCTAssert(num == 0)
-			finish.fulfill()
-		})
+		Random.Default.randomInt(0...9)
+			.fork(.init(.init(gen)), absurd, { num in
+				XCTAssert(num == 0)
+				finish.fulfill()
+			})
+		
+		waitForExpectations(timeout: 1, handler: nil)
+	}
+	
+	func testRandomId() {
+		let finish = expectation(description: "finished")
+
+		let rnd = Random()
+		let gen = LCRNG(seed: 1)
+
+		let segment = rnd.oneOf([
+			rnd.digit(),
+			rnd.uppercaseLetter()
+		])
+			.replicateM(6)
+			.map { $0.joined() }
+		
+		let id = segment
+			.replicateM(3)
+			.map { $0.joined(separator: "-") }
+			
+		id
+			.fork(.init(.init(gen)), {
+				XCTFail()
+			}, { id in
+				XCTAssert(id == "515995-777315-555151")
+				finish.fulfill()
+			})
 		
 		waitForExpectations(timeout: 1, handler: nil)
 	}
