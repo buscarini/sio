fb547c9deb080756d32b7112950b3d77e3d37dca
José Manuel Sánchez <buscarini@gmail.com>
2020-03-28 00:57:58 +0100

    Fix

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
 	name: "Sio",
-	type: .dynamic,
 	platforms: [
 		.macOS(.v10_11), .iOS(.v10),
 	],
@@ -13,22 +12,27 @@ let package = Package(
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
 			name: "Sio",
+			type: .dynamic,
 			targets: ["Sio"]
 		),
 		.library(
 			name: "SioCodec",
+			type: .dynamic,
 			targets: ["SioCodec"]
 		),
 		.library(
 			name: "SioValueStore",
+			type: .dynamic,
 			targets: ["SioValueStore"]
 		),
 		.library(
 			name: "SioEffects",
+			type: .dynamic,
 			targets: ["SioEffects"]
 		),
 		.library(
 			name: "SioNetwork",
+			type: .dynamic,
 			targets: ["SioNetwork"]
 		)
 	],
