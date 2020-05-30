1825efc0ef35fb7d380ad9616a11d6c8322944d6
José Manuel Sánchez <buscarini@gmail.com>
2020-05-30 20:49:13 +0200

    Added combine extension to get a future from a sio

---
+++ Sources/Sio/SIO+Combine.swift
@@ -0,0 +1,21 @@
+//
+//  SIO+Combine.swift
+//  SioCodecTests
+//
+//  Created by José Manuel Sánchez Peñarroja on 30/05/2020.
+//
+
+import Foundation
+import Combine
+
+@available(iOS 13.0, *)
+@available(OSX 10.15, *)
+extension SIO where R == Void, E: Error {
+	var future: Future<A, E> {
+		Future<A, E> { promise in
+			self
+				.result()
+				.run(promise)
+		}
+	}
+}
