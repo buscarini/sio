1b4f72eeee7a834bbb969ace4f4114e09717cf71
José Manuel Sánchez <buscarini@gmail.com>
2020-07-22 08:56:23 +0200

    Fix

--- Sources/SioCodec/Codec+LosslessStringConvertible.swift
+++ Sources/SioCodec/Codec+LosslessStringConvertible.swift
@@ -8,12 +8,12 @@
 import Foundation
 import Sio
 
-public extension Codec where E == Void, A == LosslessStringConvertible, B: String {
+public extension Codec where E == Void, A: LosslessStringConvertible, B == String {
 	static var stringConvertible: Codec<Void, A, B> {
-		Codec<Void, A, B>(to: { string in
-			.right(b.description)
-		}, from: { b in
-			Either.from(B(string), ())
+		Codec<Void, A, B>(to: { a in
+			.right(a.description)
+		}, from: { string in
+			Either.from(A(string), ())
 		})
 	}
 }
