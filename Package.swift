39f9aabbb2523c6bff210e3591f17dbefb531607
José Manuel Sánchez <buscarini@gmail.com>
2020-09-23 16:33:41 +0200

    Removed dynamic type to try to fix issues with Xcode 12

--- Package.swift
+++ Package.swift
@@ -12,27 +12,22 @@ let package = Package(
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
 			name: "Sio",
-			type: .dynamic,
 			targets: ["Sio"]
 		),
 		.library(
 			name: "SioCodec",
-			type: .dynamic,
 			targets: ["SioCodec"]
 		),
 		.library(
 			name: "SioValueStore",
-			type: .dynamic,
 			targets: ["SioValueStore"]
 		),
 		.library(
 			name: "SioEffects",
-			type: .dynamic,
 			targets: ["SioEffects"]
 		),
 		.library(
 			name: "SioNetwork",
-			type: .dynamic,
 			targets: ["SioNetwork"]
 		)
 	],
