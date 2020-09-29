721392260c72ad499d17bca686fceeb2035179e7
José Manuel Sánchez <buscarini@gmail.com>
2020-09-29 10:13:24 +0200

    Removed resources for now

--- Package.swift
+++ Package.swift
@@ -34,9 +34,9 @@ let package = Package(
 	dependencies: [
 	  .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.2")
 	],
-	resources: [
-		.copy("Tests/SioEffectsTests/Resource.txt")
-	],
+//	resources: [
+//		.copy("Tests/SioEffectsTests/Resource.txt")
+//	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
