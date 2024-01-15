ae8ad20dc21c5c127ff6633ee6607b33f47a19d3
José Manuel Sánchez <buscarini@gmail.com>
2024-01-15 10:26:14 +0100

    config: Updated package platforms and podspec version.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "Sio",
 	platforms: [
-		.macOS(.v10_11), .iOS(.v10),
+		.macOS(.v10_11), .iOS(.v10), .tvOS(.v10)
 	],
 	products: [
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
