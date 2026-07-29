eafc4b96798b937ef9b565a2df7027a916faca7d
José Manuel Sánchez <jose.sanchez@tyris-software.com>
2026-07-29 11:18:42 +0200

    breaking: Removed Task alias.

--- Sources/Sio/SIO+Typealias.swift
+++ Sources/Sio/SIO+Typealias.swift
@@ -3,6 +3,3 @@ import Foundation
 public typealias UIO<A> = SIO<Void, Never, A>
 public typealias IO<E, A> = SIO<Void, E, A>
 public typealias TaskR<R, A> = SIO<R, Error, A>
-
-@available(*, deprecated, message: "Use an explicit name instead. Deprecated due to naming collision with Swift concurrency.")
-public typealias Task<A> = SIO<Void, Error, A>
